5c976408b950391c22235589824d06785788d091
jaysonkunkel <jayson.kunkel@gmail.com>
2023-10-31 00:31:19 -0500

    implemented insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,9 +1,10 @@
 import java.util.Comparator;
 
 /**
- * Sort using insertion sort.
+ * A sorting algorithm that divides a collection into sorted and unsorted values, and
+ * "inserts" each unsorted value into its correct place in the sorted values.
  *
- * @author Your Name Here
+ * @author Jayson Kunkel
  */
 
 public class InsertionSort implements Sorter {
@@ -31,8 +32,44 @@ public class InsertionSort implements Sorter {
   // | Methods |
   // +---------+
 
+  /**
+   * Swaps two elements in an array.
+   */
+  public static <T> void swap (T[] vals, int i, int j){
+    T temp = vals[i];
+    vals[i] = vals[j];
+    vals[j] = temp;
+  } // swap (T[] int, int)
+
+  
   @Override
-  public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
+  public <T> void sort(T[] values, Comparator<? super T> comparator) {
+    sort(values, comparator, 0, values.length);
   } // sort(T[], Comparator<? super T>
+
+   /**
+   * Sort the values in indices [lb..ub) of values using order to compare values.
+   */
+  private static <T> void sort(T[] values, Comparator<? super T> comparator, int lb, int ub){
+
+    // initialize the counter; one-element subarrays are sorted
+    int i = lb + 1;
+
+    while(i < ub){
+      // if the current element is smaller than the previous element
+      if(comparator.compare(values[i], values[i-1]) <= 0){
+
+        int j = 0;
+        // repeatedly swap out of place element to the left until it is in place
+        while(j <= i){
+          if(comparator.compare(values[i], values[j]) <= 0){
+            swap(values, i, j);
+          } // if
+          j++;
+        } // while
+      } // if
+      i++;
+    } // while
+  } // sort(T[], Comparator<? super T>, int, int)
+
 } // class InsertionSort
