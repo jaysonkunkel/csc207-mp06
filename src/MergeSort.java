bd6c6c62adc4a2c4108391bbafeb14f7d1db2b99
jaysonkunkel <jayson.kunkel@gmail.com>
2023-10-31 00:31:34 -0500

    implemented merge sort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,9 +1,11 @@
 import java.util.Comparator;
+import java.util.Arrays;
 
 /**
- * Sort using merge sort.
+ * A sorting algorithm that divides a collection in half, recursively
+ * sorts both halves, and merges them back together.
  *
- * @author Your Name Here
+ * @author Jayson Kunkel
  */
 
 public class MergeSort implements Sorter {
@@ -31,8 +33,73 @@ public class MergeSort implements Sorter {
   // | Methods |
   // +---------+
 
+  // +------------------+--------------------------------------------
+  // | Exported methods |
+  // +------------------+
+
+  /**
+   * Sort an array using the merge sort algorithm.
+   */
   @Override
-  public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
-  } // sort(T[], Comparator<? super T>
+  public <T> void sort(T[] vals, Comparator<? super T> comparator) {
+    sort(vals, comparator, 0, vals.length-1);
+  } // sort
+
+  public static <T> void sort(T[] vals, Comparator<? super T> comparator, int lo, int hi){
+
+    if(lo < hi){
+
+      int mid = (lo + hi)/2;
+
+      sort(vals, comparator, lo, mid);
+      sort(vals, comparator, mid+1, hi);
+
+      merge(vals, lo, mid, hi, comparator);
+    }
+
+  } // sort
+
+  // +-----------------+---------------------------------------------
+  // | Local utilities |
+  // +-----------------+
+
+  /**
+   * Merge the values from positions [lo..mid) and [mid..hi) back into
+   * the same part of the array.
+   *
+   * Preconditions: Each subarray is sorted accorting to comparator.
+   */
+  static <T> void merge(T[] vals, int lo, int mid, int hi, Comparator<? super T> comparator) {
+
+    T[] merged = java.util.Arrays.copyOfRange(vals, lo, hi+1);
+
+    System.out.println(Arrays.toString(merged));
+
+    int left = lo;
+    int right = mid+1;
+    int current = lo;
+
+    // Merge the left and right halves
+    while(left <= lo + (mid-lo) && right <= lo + (hi - lo)){
+      if(comparator.compare(merged[left-lo], merged[right-lo]) <= 0){
+        vals[current] = merged[left-lo];
+        left++;
+      }
+      else{
+        vals[current] = merged[right-lo];
+        right++;
+      }
+      current++;
+    }
+
+    // Copy over remaining elements, if any
+    while(left <= lo + (mid-lo)){
+      vals[current++] = merged[left++ - lo];
+    }
+
+    while(right < hi - mid){
+      vals[current++] = merged[right++ - lo];
+    }
+  } // merge
+  
 } // class MergeSort
